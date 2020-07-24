92a4c637ab6e824f4ef701cdbf08e6f4a363192a
William Gonçalves <williamcco@gmail.com>
2020-07-24 20:37:21 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks', 'jsx-a11y', 'import'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+> :skull: Template repository for ReactJS projects &mdash; using [CRA](https://github.com/facebook/create-react-app), ESLint (with [Airbnb's style guide](https://github.com/airbnb/javascript)), Prettier, EditorConfig and root based imports.
+
+Install the dependencies using `yarn` or `npm install`.
